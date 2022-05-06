694fde79a5573203973d098fb2913bd96fe5d763
Daniel Morales <goodluck1031@outlook.com>
2022-05-06 08:39:14 -0700

    function and inputs

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,55 @@ fn main() {
 
     println!("The value of 10 exist in the array {}", number_vec.contains(&10));
 
+    // function
+    basic_fn();
+
+    function_with_inputs("Nouman", 40_000);
+
+    let full_name = "Kamran";
+    let salary = 50_000;
+
+    function_with_inputs(full_name, salary);
+
+    let answer = function_with_inputs_outputs(10, 15);
+    println!("The answer of multiplication is {}", answer);
+
+    let (multi, addi, subt) = function_with_inputs_multiple_outputs(10, 15);
+    println!("Mulitplication: {}, addition = {}, subtraction = {}", multi, addi, subt);
+
+    let results = function_with_inputs_multiple_outputs(10, 15);
+    println!("Multiplication = {}, addition = {}, subtraction = {}", results.0, results.1, results.2);
+
+    let full_name = {
+        let first = "Nouman";
+        let last_name = "Azam";
+        format!("{} {}", first, last_name)
+    };
+
+    println!("My full name is {}", full_name);
+
+    // Inputs from users
+    let mut n = String::new();
+    std::io::stdin()
+        .read_line(&mut n)
+        .expect("Failed to read input.");
+
+    let n:f64 = n.trim().parse().expect("invalid input");
+    println!("{:?}", n);
 }
 
+fn basic_fn() {
+    println!("This is  a basic function");
+}
 
+fn function_with_inputs(name: &str, salary: i32) {
+    println!("The name is {} and the salary is {}", name, salary);
+}
+
+fn function_with_inputs_outputs(num1: i32, num2: i32) -> i32 {
+    num1 * num2
+}
+
+fn function_with_inputs_multiple_outputs(num1: i32, num2: i32) -> (i32, i32, i32) {
+    (num1 * num2, num1 + num2, num1 - num2)
+}
