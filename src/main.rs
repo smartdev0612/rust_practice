3eacea1d4cf10fc3066aa52107547f9b6c31a34f
Daniel Morales <goodluck1031@outlook.com>
2022-05-06 08:01:40 -0700

    vector

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,36 @@
 fn main() {
-    // Initializing multiple variables
-    let (first_number, second_number) = (250, 480.32);
-    println!("The first number is {} and the second number is {}", first_number, second_number);
+    // Vectors
+    let mut number_vec:Vec<i32> = vec![5, 6, 7, 8, 9, 10, 11, 12, 13, 16];
+    println!("{}", number_vec[0]);
+    number_vec[4] = 5;
+    println!("{:?}", number_vec);
 
-    // Readability of large number
-    let large_number = 1_000_000;
-    println!("The value of the large number is {}", large_number);
-    
-    // Integer overflow
-    // let overflow_number:u8 = 256;
+    let array_with_some_elements:Vec<i32> = vec![0;10];
 
-    // Decimal numbers in other formats
-    let x = 255;
-    println!("The value of variable x in hexadecimal is {:o} and in octal is {:X} and in binary {:b}", x, x, x);
+    let mut string_array_1:Vec<&str> = vec!["apple", "tomato", "grapes"];
+    string_array_1[0] = "Kamran Azam";
 
-    // Snake case convention
-    let number = 45;
+    let string_array_2:Vec<&str> = vec!["Unknown"; 6];
 
-    // Operations on number in different formats
-    let n1 = 14;
-    let n2 = 15.6;
-    let n3 = n1 as f64 + n2;
-    println!("{}", n3);
+    let char_vec:Vec<char> = vec!['a', 'p', 'p', 'l', 'e'];
 
-    // shadowing
-    let s = 5;
-    let s = 5 * 5;
-    println!("the value of the variable s = {}", s);
+    let subset_vec = &number_vec[0..3];
+    println!("the subset of the values of the array {:?}", subset_vec);
 
-    // string
-    let some_string = "Fixed Length string";
-    println!("The text inside the string is \"{}\"", some_string);
+    println!("Elements in the array are {}", number_vec.len());
 
-    let my_name = "Nouman azam".to_string();
+    let check_index = number_vec.get(2);
+    println!("{:?}", check_index);
 
-    let p1 = (2.5, 3.5);
-    let p2 = (1.5, 5.5);
+    number_vec.push(30);
+    number_vec.push(31);
+    println!("The value are the vector are {:?}", number_vec);
 
-    println!("Diff X: {}", (p1.0 - p2.0 as f64).abs());
-    println!("Diff Y: {}", (p1.1 - p2.1 as f64).abs());
+    number_vec.remove(5);
+    println!("The value after removing are {:?}", number_vec);
 
-    let p3 = [2.5, 3.5];
-    let p4 = [1.5, 5.7];
+    println!("The value of 10 exist in the array {}", number_vec.contains(&10));
 
-    println!("Diff X: {}", (p3[0] - p4[0] as f64).abs());
-    println!("Diff Y: {}", (p3[1] - p4[1] as f64).abs());
-
-    let p5 = (4.0, 3.0);
-    let p6 = (5.0, 4.5);
-    let distance = (((p5.0 - p6.0) as f64).powf(2.0) + ((p5.1 - p6.1) as f64).powf(2.0)).sqrt();
-
-    println!("Distance: {}", distance);
 }
 
 
