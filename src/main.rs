66f5ddab37b97defc95fdb9bd1fd5d5157463d6f
Daniel Morales <goodluck1031@outlook.com>
2022-05-06 07:34:49 -0700

    tuple, array, slice

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,56 @@
 fn main() {
-    // Variables in Rust
-    let mut x:i64 = 15;
-    println!("The value of the variable x = {}", x);
-    x = 60;
+    // Initializing multiple variables
+    let (first_number, second_number) = (250, 480.32);
+    println!("The first number is {} and the second number is {}", first_number, second_number);
 
-    // Varialbe Type
-    println!("The maximum number in i8 is equal to {}", std::i8::MAX);
-    println!("The maximum number in u8 is equal to {}", std::u8::MAX);
+    // Readability of large number
+    let large_number = 1_000_000;
+    println!("The value of the large number is {}", large_number);
+    
+    // Integer overflow
+    // let overflow_number:u8 = 256;
 
-    // Floats
-    let z = 3.65;
-    println!("The maximum number in f32 is {}", std::f32::MAX);
+    // Decimal numbers in other formats
+    let x = 255;
+    println!("The value of variable x in hexadecimal is {:o} and in octal is {:X} and in binary {:b}", x, x, x);
 
-    // Boolean
-    let status = false;
-    println!("The values of the some of our variables are {:?}", (x, z, status));
+    // Snake case convention
+    let number = 45;
 
-    let not_equals = 18 != 18;
-    println!("The value of condition 18 != 18 is {}", not_equals);
+    // Operations on number in different formats
+    let n1 = 14;
+    let n2 = 15.6;
+    let n3 = n1 as f64 + n2;
+    println!("{}", n3);
 
-    // Characters
-    let c1 = 'a';
-    let c2 = '3';
-    let c3 = '+';
-    let c4 = '\u{288A}';
-    let c5 = '\"';
-    println!("The value of c1 = {}, c2 = {}, c3 = {}, c4 = {}, c5 = {}", c1, c2, c3, c4, c5);
+    // shadowing
+    let s = 5;
+    let s = 5 * 5;
+    println!("the value of the variable s = {}", s);
 
-    let my_age = 40;
-    let my_son_age = 35;
-    println!("My age is {} and my son age is {}", my_age, my_son_age);
+    // string
+    let some_string = "Fixed Length string";
+    println!("The text inside the string is \"{}\"", some_string);
 
-    let x1 = 40;
-    let mut x2 = x1;
-    x2 = x1 - 2;
-    println!("My age is {} and my son age is {}", x1, x2);
+    let my_name = "Nouman azam".to_string();
 
-    // let mut x1 = 40;
-    // let x2;
-    // x1 = x1 * 3;
-    // x2 = x1 - 2;
-    // println!("My age is {} and my son age is {}", x1, x2);
+    let p1 = (2.5, 3.5);
+    let p2 = (1.5, 5.5);
 
+    println!("Diff X: {}", (p1.0 - p2.0 as f64).abs());
+    println!("Diff Y: {}", (p1.1 - p2.1 as f64).abs());
+
+    let p3 = [2.5, 3.5];
+    let p4 = [1.5, 5.7];
+
+    println!("Diff X: {}", (p3[0] - p4[0] as f64).abs());
+    println!("Diff Y: {}", (p3[1] - p4[1] as f64).abs());
+
+    let p5 = (4.0, 3.0);
+    let p6 = (5.0, 4.5);
+    let distance = (((p5.0 - p6.0) as f64).powf(2.0) + ((p5.1 - p6.1) as f64).powf(2.0)).sqrt();
+
+    println!("Distance: {}", distance);
 }
 
 
