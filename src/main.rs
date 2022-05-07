a5f7e29b0c517e4f586320300a93ee01b9e82df1
Daniel Morales <goodluck1031@outlook.com>
2022-05-06 21:32:21 -0700

    string concatenation

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,52 @@ fn main() {
     let ref1 = heap_num;
     let ref2 = &ref1;
 
+    // let mut heap_num = vec![4, 5, 6];
+    // let ref1 = &mut heap_num;
+    // let ref2 = &mut heap_num;
+    // println!("the first reference is {:?} and the second one is {:?}", ref1, ref2);
+
+    // let mut heap_num = vec![4, 5, 6];
+    // let ref1 = &heap_num;
+    // let ref2 = &heap_num;
+    // println!("The first references is {:?} and the second one is {:?}", ref1, ref2);
+
+    // let mut heap_num = vec![4, 5, 6];
+    // let ref1 = &heap_num;
+    // let ref2 = &heap_num;
+    // let ref3 = &mut heap_num;
+    // println!("Immutable references are {:?} and {:?} and the mutalbe reference is {:?}", ref1, ref2, ref3);
+
+    // let mut heap_num = vec![4, 5, 6];
+    // let ref1 = &heap_num;
+    // let ref2 = &heap_num;
+    // println!("Immutable references are {:?} and {:?}", ref1, ref2);
+
+    // let ref3 = &mut heap_num;
+
+    let mut heap_num = vec![4, 5, 6];
+    heap_num.push(86);
+    let ref1 = &heap_num;
+    let ref2 = &heap_num;
+    println!("Immutalbe references are {:?} and {:?}", ref1, ref2);
+
+    heap_num.push(86);
+
+    // String concatenation and ownership
+    let s1 = String::from("Hello");
+    let s2 = String::from("World");
+    let s3 = s1 + &s2;
+    println!("{} {}", s3, s2);
+
+    let s1:String = String::from("this is me");
+    let s2: &str = "myself";
+
+    some_function(&s1,s2); 
+    println!("{} {}",s1,s2);
+}
+
+fn some_function(a1: &String, a2: &str){
+    println!("{} {}",a1,a2); 
 }
 
 fn stack_function(mut var:i32) {
