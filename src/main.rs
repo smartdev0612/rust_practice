0b77f25b79522ddaffbf35378081c9f67a047a5f
Daniel Morales <goodluck1031@outlook.com>
2022-05-09 07:38:21 -0700

    lifetime

--- src/main.rs
+++ src/main.rs
@@ -193,6 +193,7 @@ fn main() {
 }
 */
 
+/*
 fn main() {
     let some_vec:Vec<i32> = vec![4,5,6,7,7,4,3,5,6];
     let mut freq_vec:HashMap<i32, u32> = HashMap::new();
@@ -201,4 +202,131 @@ fn main() {
         *freq += 1;
     }
     println!("{:?}", freq_vec);
+}
+*/
+
+//------------------------------------
+// Lifetimes
+//  - Dangling Reference
+//  - Undetermined lifetimes
+//  - Generic Lifetimes Parameters
+//  - GLP typically needed with outputs from functions that are reference
+//  - GLP with multiple variables
+//  - GLP and structures
+//  - Reference to same variable
+//------------------------------------
+
+/*
+fn main() {
+    let i:&i32;
+    {
+        let j = 5;
+        i = &j;
+    }
+    println!("The value of i = {}", i);
+} */
+
+/*
+fn main() {
+    let some_int = 10;
+    let additional_int = some_fn(&some_int);
+    println!("{}", additional_int);
+}
+
+fn some_fn(i:&i32) -> &i32 {
+    &i
+} */
+
+/*
+fn main() {
+    let int1 = 5;
+    let int2 = 10;
+}
+
+fn greater(i:&i32, j:&i32) -> &i32 {
+    if i > j{
+        i
+    } else {
+        j
+    }
+}
+*/
+
+/*
+fn main() {
+    let s_1 = "Hello";
+    let v;
+    {
+        let s_2 = String::from("world");
+        v = some_fn(s_1, s_2.as_str());
+    }
+    println!("{}", v);
+}
+
+fn some_fn<'a, 'b>(first_str:&'a str, second_str:&'b str) -> &'a str {
+    first_str
+}
+*/
+
+/* 
+fn main() {
+    let int1 = 5;
+    let int2 = 10;
+    let result = greater(&int1, int2);
+}
+
+fn greater<'a>(i:&'a i32, j:i32) -> &'a i32 {
+    i
+} */
+
+/*
+fn main() {
+    let int1 = 5;
+    {
+        let int2 = 10;
+        let result = greater(&int1, &int2);
+        println!("the larger value is {}", result);
+    }
+}
+
+fn greater<'a, 'b>(i:&'a i32, j:&'a i32) -> &'a i32 {
+    if i > j {
+        i
+    } else {
+        j
+    }
+}
+*/
+
+/* 
+struct Person<'a> {
+    name: &'a str,
+    age: i32,
+}
+
+fn main() {
+    let first_name = "Nouman";
+    let mut nouman = Person {
+        name: &first_name,
+        age: 40,
+    };
+
+    {
+        let last_name = String::from("Azam");
+        nouman.name = &last_name;
+    }
+    println!("the name of the person is {} and his age is {}", nouman.name, nouman.age);
+} */
+
+fn main() {
+    let some_vec = vec![5, 7, 8, 9, 7, 9, 5, 2];
+    let return_vec = use_vec(&some_vec, &some_vec);
+}
+
+fn use_vec<'a>(vec1: &'a [i32], vec2: &'a [i32]) -> &'a [i32] {
+    if 3 > 5 {
+        vec1
+    } else {
+        vec2
+    }
 }
