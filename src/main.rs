4398ca24c3d3bc076e4d16100c2b70a45c4ed1f7
Daniel Morales <goodluck1031@outlook.com>
2022-05-08 18:06:29 -0700

    Trait

--- src/main.rs
+++ src/main.rs
@@ -183,6 +183,7 @@ fn main() {
     }
     */
 
+    /*
     let input_string = String::from("Welcome to Rust");
     let size_stack = input_string.len();
     let mut stack = new_stack(size_stack);
@@ -198,7 +199,163 @@ fn main() {
 
     println!("The input string is {:?}", input_string);
     println!("The reverse of the string is {:?}", rev_string);
+    */
+
+    // Traits
+    /*
+    let person1 = Person {
+        name: String::from("Nouman Azam"),
+        citizenship: String::from("Pakistan"),
+        age: 40,
+        gender: 'M',
+        salary: 40_000,
+    };
+
+    let student1 = Student {
+        name_std: String::from("Affan Azam"),
+        age: 15,
+        sex: 'M',
+        country: String::from("USA"),
+    };
+
+    println!("The basic info include {:?}", person1.info());
+    println!("The basic info for the student is {:?}", student1.info());
+    
+
+    let c1 = Circle{
+        radius: 3.2,
+    };
+
+    let r1 = Rectangle {
+        width: 5.0,
+        length: 4.0
+    };
+
+    c1.area();
+    c1.parameter();
+
+    r1.area();
+    r1.parameter();
+    */
+
+    let my_data = Data {
+        some_data: vec![5, 6, 7, 8, 9, 10,3, 5, 4],
+    };
+
+    println!("The mean of the data is {}", my_data.mean());
+    println!("The variance of the data is {}", my_data.variance());
 }
+
+struct Data {
+    some_data: Vec<i32>,
+}
+
+struct Circle {
+    radius: f32,
+}
+
+struct Rectangle {
+    length: f32,
+    width: f32,
+}
+
+trait BasicStats{
+    fn mean(&self) -> f32;
+    fn variance(&self) -> f32;
+}
+
+impl BasicStats for Data {
+    fn mean(&self) -> f32 {
+        let mut sum = 0;
+        for i in self.some_data.iter() {
+            sum = sum + i;
+        }
+        sum as f32 / self.some_data.len() as f32
+    }
+
+    fn variance(&self) -> f32 {
+        let mu = self.mean();
+        let mut sum_sqared_diff = 0.0;
+        for i in self.some_data.iter() {
+            sum_sqared_diff += (*i as f32 - mu) * (*i as f32 - mu);
+        }
+        sum_sqared_diff / self.some_data.len() as f32
+    }
+}
+
+trait General_info {
+    fn area(&self){
+        println!("I am not implemented for this type");
+    }
+
+    fn parameter(&self);
+}
+
+impl General_info for Circle {
+    // fn area(&self) {
+    //     let area_of_circle = 3.14 * (self.radius * self.radius);
+    //     println!("The area of the circle is {}", area_of_circle);
+    // }
+
+    fn parameter(&self) {
+        let circumference = 2.0 * 3.14 * self.radius;
+        println!("the circumference of the circle is {}", circumference);
+    }
+}
+
+impl General_info for Rectangle {
+    // fn area(&self) {
+    //     let area_of_rect = self.length * self.width;
+    //     println!("The area of the rectangle is {}", area_of_rect);
+    // }
+
+    fn parameter(&self) {
+        let parameter_rect = 2.0 * (self.length + self.width);
+        println!("The area of the reactangle is {}", parameter_rect);
+    }
+}
+
+/*
+struct Person {
+    citizenship: String,
+    name: String,
+    age: u8,
+    gender: char,
+    salary: i32,
+}
+
+struct Student {
+    name_std: String,
+    age: u8,
+    sex: char,
+    country: String,
+}
+
+trait General_info {
+    fn info(&self) -> (&str, u8, char);
+    fn country_info(&self) -> &str;
+}
+
+impl General_info for Person {
+    fn info(&self) -> (&str, u8, char) {
+        (&(self.name), self.age, self.gender)
+    }
+
+    fn country_info(&self) -> &str {
+        &self.name
+    }
+}
+
+impl General_info for Student {
+    fn info(&self) -> (&str, u8, char) {
+        (&self.name_std, self.age, self.sex)
+    }
+
+    fn country_info(&self) -> &str {
+        &self.name_std
+    }
+}
+*/
 
 fn new_stack(maxsize: usize) -> Vec<char> {
     let vec: Vec<char> = Vec::with_capacity(maxsize);
