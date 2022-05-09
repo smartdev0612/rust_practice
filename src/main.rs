33b6b7fa9495eccfa8081cb446a2a3914ef35703
Daniel Morales <goodluck1031@outlook.com>
2022-05-09 09:47:29 -0700

    iterators

--- src/main.rs
+++ src/main.rs
@@ -466,7 +466,7 @@ fn main() {
     let (my_name, my_age) = (String::from("Nouman"), 40);
     prints_full_info(prints_name, &my_name, my_age);
 }
-*/
+
 
 fn add_one(x: i32) -> i32 {
     x + 1
@@ -480,10 +480,66 @@ fn main() {
     let answer = do_twice(add_one, 5);
     println!("The answer is: {}", answer);
 }
+*/
 
+//------------------------------------
+// Iterators
+//  - Basics of iterators
+//  - some useful functions for iterators
+//  - Common statistics related functions
+//  - Modifying and collecting values
+//------------------------------------
 
+fn main() {
+    /*
+    let some_vec = vec![1,2,3,4,5,6,7,8];
+    let mut iter = some_vec.iter();
 
+    println!("The iterator : {:?}", iter);
+    println!("{:?}", iter.next());
+    println!("{:?}", iter.next());
+    
 
+    let a:Vec<u32> = vec![0,1,2,3,4,5,6,7,8];
+    let mut check = a.iter().any(|&x| x > 0);
+    println!("The value of the any function is {}", check);
+
+    let check = a.iter().all(|&x| x >= 0);
+    println!("The value of the all function is {}", check);
+
+    let check = a.iter().find(|&&x| x > 0);
+    println!("The value of the function is {}", check.unwrap());
+
+    let check = a.iter().position(|&x| x > 4);
+    println!("The value of the function position is {}", check.unwrap());
+
+    let check = a.iter().rposition(|&x|x > 4);
+    println!("The value of the functon rpostion is {}", check.unwrap());
+
+    let check = a.iter().max();
+    println!("The value of the function max is {}", check.unwrap());
+
+    let check = a.iter().min();
+    println!("The value of the function min is {}", check.unwrap());
+
+    let mut iter = a.iter().rev();
+    println!("The result of applying the rev function {:?}", iter.next());
+    */
+
+    let a = vec![0,1,2,3,4,5,6,7];
+    let filtered_values = a.iter().filter(|&x| *x >= 5).collect::<Vec<&u32>>();
+    println!("{:?}", filtered_values);
+
+    let b = a.clone();
+    let filtered_values = a.into_iter().filter(|x| *x >= 5).collect::<Vec<u32>>();
+    println!("{:?}", filtered_values);
+
+    let mapped_values = b.iter().map(|x| 2 * *x).collect::<Vec<u32>>();
+    println!("{:?}", mapped_values);
+
+    let mapped_values = b.iter().map(|x| 2 * *x).filter(|x| *x > 10).collect::<Vec<u32>>();
+    println!("{:?}", mapped_values);
+}
 
 
 
