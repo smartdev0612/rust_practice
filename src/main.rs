711d2b60b9dcfc0c6f673b8712872655ba546766
Daniel Morales <goodluck1031@outlook.com>
2022-05-09 03:20:31 -0700

    result enum

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,8 @@ fn main() {
 }
 */
 
+// Generics
+/*
 struct Point<T, U> {
     x: T,
     y: U,
@@ -55,4 +57,96 @@ fn main() {
     };
 
     p1.printing();
+}
+*/
+
+// Option Enum
+
+// enum Option<T> {
+//     None,
+//     Some(T),
+// }
+
+/*
+fn main() {
+    let mut disease:Option<String> = None;
+    disease = Some(String::from("Diabetes"));
+
+    match disease {
+        Some(disease_name) => println!("You have the disease of {}", disease_name),
+        None => println!("You do not have any disease"),
+    }
+}
+*/
+
+/*
+fn main() {
+    let s1:Option<&str> = Some("Some String");
+    println!("The value of s1 is {:?}\n the value of s1 itself is {:?}", s1, s1.unwrap());
+
+    let f1:Option<f64> = Some(10.54);
+    let mut f2 = 16.5;
+    f2 = f2 + f1.unwrap();
+
+    println!("The value of the sum is {}", f2);
+
+    let v1:Option<Vec<i32>> = Some(vec![0,1,2,3]);
+
+    let p1 = Person {
+        name:String::from("Nouman"),
+        age: 32,
+    };
+    let some_one:Option<Person> = Some(p1);
+}
+
+struct Person {
+    name: String,
+    age: i32,
+}
+
+
+fn square(num: Option<i32>) -> Option<i32> {
+    match num {
+        Some(number) => Some(number * number),
+        None => None,
+    }
+}
+
+fn main() {
+    let number = Some(6);
+    if square(number) != None {
+        println!("the result of the square is {:?}", square(Some(6)).unwrap());
+    } else {
+        println!("we do not have any values");
+    }
+}
+*/
+
+// Result Enum
+/*fn division(divident: f64, divisor:f64) -> Result<f64, String> {
+    /*if divisor == 0.0 {
+        Err(String::from("Error: Division by zero"))
+    } else {
+        Ok(divident / divisor)
+    }*/
+
+    match divisor {
+        0. => Err(String::from("Error: Division by zero")),
+        _ => Ok(divident / divisor)
+    }
+}
+
+fn main() {
+    println!("{:?}", division(9.0, 3.0));
+    println!("{:?}", division(4.0, 0.0));
+    println!("{:?}", division(0.0, 2.0));
+}*/
+
+fn main() {
+    let some_vec = vec![5,5,4,3,2,3,7,8];
+    let result = match some_vec.get(5) {
+        Some(a) => Ok(a),
+        None => Err("the value does not exist"), 
+    };
+    println!("The value of the result is {:?}", result);
 }
